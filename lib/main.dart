87d94b0ed746c0ae8212aeb14afaf4332dc39964
Natasha Trayers <ntrayers@gmail.com>
2021-11-08 10:58:56 -0600

    use device_preview to test different screen sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app1/pages/appointment_info.dart';
 import 'package:flutter_app1/pages/home.dart';
@@ -5,13 +7,18 @@ import 'package:flutter_app1/pages/loading.dart';
 import 'package:flutter_app1/pages/take_photo_screen.dart';
 
 main() {
-  runApp(MaterialApp(
-    initialRoute: '/home',
-    routes: {
-      '/home': (context) => Home(),
-      '/loading': (context) => Loading(),
-      '/info': (context) => Info(),
-      '/take_photo': (context) => TakePhoto(),
-    },
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => Home(),
+        '/loading': (context) => Loading(),
+        '/info': (context) => Info(),
+        '/take_photo': (context) => TakePhoto(),
+      },
+    ),
   ));
 }
