ea2fbf830497162ac6a3e6ed3fb4711eaff666d2
shanmukanaks <47695958+shanmukanaks@users.noreply.github.com>
2021-11-14 21:10:20 -0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app1/pages/appointment_info.dart';
 import 'package:flutter_app1/pages/home.dart';
 import 'package:flutter_app1/pages/loading.dart';
+import 'package:flutter_app1/pages/splash.dart';
 import 'package:flutter_app1/pages/take_photo_screen.dart';
 
 main() {
   runApp(MaterialApp(
-    initialRoute: '/home',
+    initialRoute: '/splash',
     routes: {
+      '/splash': (context) => SplashScreen(),
       '/home': (context) => Home(),
       '/loading': (context) => Loading(),
       '/info': (context) => Info(),
