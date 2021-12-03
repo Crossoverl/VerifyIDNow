dd28ea8d21f9e7ed4ec841023ed3e555cdb0a3a7
Patrick Lombardi <plombardignr@gmail.com>
2021-12-03 14:26:45 -0600

    Info page back button bug fix

--- lib/pages/appointment_info.dart
+++ lib/pages/appointment_info.dart
@@ -9,49 +9,47 @@ Future main() async {
     DeviceOrientation.portraitDown,
   ]);
 
-  runApp(Info());
+  runApp(InfoPage());
 }
 
-class Info extends StatelessWidget {
-  const Info({Key? key}) : super(key: key);
-  static final String title = 'Information';
+// class Info extends StatelessWidget {
+//   const Info({Key? key}) : super(key: key);
+//   static final String title = 'Information';
+//
+//   @override
+//   Widget build(BuildContext context) => MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: title,
+//         theme: ThemeData(
+//           primaryColor: Colors.black,
+//         ),
+//         home: InfoPage(title: title),
+//       );
+// }
+
+class InfoPage extends StatefulWidget {
+  final String title = 'Information';
+
+  const InfoPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: title,
-        theme: ThemeData(
-          primaryColor: Colors.black,
-        ),
-        home: MainPage(title: title),
-      );
+  _InfoPageState createState() => _InfoPageState();
 }
 
-class MainPage extends StatefulWidget {
-  final String title;
-
-  const MainPage({
-    required this.title,
-  });
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int index = 0;
+class _InfoPageState extends State<InfoPage> {
+  // int index = 0;
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
           backgroundColor: Colors.lightBlue,
-          // leading: new IconButton(
-          //   icon: new Icon(Icons.arrow_back_ios, color: Colors.grey),
-          //   onPressed: () {
-          //     Navigator.of(context, rootNavigator: true).pop(context);
-          //   },
-          // ),
+          leading: new IconButton(
+            icon: new Icon(Icons.arrow_back_ios, color: Colors.grey),
+            onPressed: () {
+              Navigator.of(context, rootNavigator: true).pop(context);
+            },
+          ),
         ),
         body: TextfieldGeneralWidget(),
       );
